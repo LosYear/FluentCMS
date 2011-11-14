29d67be1086c33d797fddb84b4c8f0789f183370
LosYear <losevyaroslav@gmail.com>
2011-11-14 10:26:08 +0400

    Forgot to add database to previous commit

--- install/base.sql
+++ install/base.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: localhost
--- Время создания: Окт 23 2011 г., 21:11
+-- Время создания: Ноя 14 2011 г., 10:24
 -- Версия сервера: 5.1.50
 -- Версия PHP: 5.3.5
 
@@ -38,6 +38,8 @@ CREATE TABLE IF NOT EXISTS `comments` (
 -- Дамп данных таблицы `comments`
 --
 
+INSERT INTO `comments` (`id`, `parent_id`, `name`, `comment`, `date_add`, `text_id`) VALUES
+(1, 0, 'LosYear', 'Мы очень рады', '2011-11-07 18:47:05', 6);
 
 -- --------------------------------------------------------
 
@@ -62,9 +64,31 @@ CREATE TABLE IF NOT EXISTS `contest` (
 --
 
 INSERT INTO `contest` (`id`, `type`, `name`, `desc`, `rootcat`, `subtype`, `from`, `till`) VALUES
-(1, 1, 'Физико-Математическое направление', 'Описание, написанное администратором', -1, -1, '2011-09-29 09:54:17', '2011-09-29 09:54:17'),
-(2, 2, 'Первый тур', 'Проходит в форме блиц', 1, 1, '2011-09-30 09:00:00', '2011-10-29 09:00:00'),
-(6, 2, 'Второй тур', 'Полный тур', 1, 2, '2011-10-12 00:00:00', '2011-10-20 00:00:00');
+(2, 2, 'Блиц тур', 'Тур представлен в виде теста, выполняется в течении часа.', 1, 1, '2011-11-07 00:00:00', '2012-11-07 00:00:00'),
+(1, 1, 'Физико-математическое направлени', 'Направление, в котором дети могут проявить себя в качестве будующих математиков и физиков', -1, -1, '2011-11-07 20:32:42', '2011-11-07 20:32:42'),
+(3, 2, 'Полный тур', 'Скачиваете задание, отправляете ответы', 1, 2, '2011-11-07 00:00:00', '2011-12-07 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `messages`
+--
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL,
+  `from` int(11) NOT NULL,
+  `to` int(11) NOT NULL,
+  `title` text CHARACTER SET utf8 NOT NULL,
+  `text` text CHARACTER SET utf8 NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `state` int(11) NOT NULL,
+  `hidden` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Дамп данных таблицы `messages`
+--
+
 
 -- --------------------------------------------------------
 
@@ -94,7 +118,8 @@ CREATE TABLE IF NOT EXISTS `modules` (
 INSERT INTO `modules` (`id`, `name`, `author`, `folder`, `title`, `description`, `version`, `cms_version_min`, `cms_version_max`, `is_active`, `main_filename`, `contacts`) VALUES
 (1, 'fl_twitter', 'LosYear aka Losev Yaroslav', 'twitter', 'FLuent CMS Twitter', 'Twitter', '0.1 alpha', '*', '*', 0, 'index.php', 'E-Mail : flexo.o@yandex.ru www : www.losyar.ru'),
 (2, 'fl_profiles', 'LosYear aka Losev Yaroslav', 'pages', 'User Profiles', 'User Profiles for Fluent. Part of Fluent Rush', '0.1 alpha', '*', '*', 1, 'index.php', 'E-Mail : flexo.o@yandex.ru www : www.losyar.ru'),
-(3, 'fl_concore', 'LosYear aka Losev Yaroslav', 'contest', 'Contest core', 'Core for contest', '0.1 alpha', '*', '*', 1, 'index.php', 'E-Mail : flexo.o@yandex.ru www : www.losyar.ru');
+(3, 'fl_concore', 'LosYear aka Losev Yaroslav', 'contest', 'Contest core', 'Core for contest', '0.1 alpha', '*', '*', 1, 'index.php', 'E-Mail : flexo.o@yandex.ru www : www.losyar.ru'),
+(4, 'fr_messages', 'LosYear aka Losev Yaroslav', 'messages', 'Fluent CMS private messages', 'Users can write messages to each other', '0.1 alpha', '*', '*', 0, 'index.php', 'E-Mail : flexo.o@yandex.ru www : www.losyar.ru');
 
 -- --------------------------------------------------------
 
@@ -137,20 +162,19 @@ CREATE TABLE IF NOT EXISTS `questions` (
   `ans4` text CHARACTER SET utf8 NOT NULL,
   `write_ans` int(11) NOT NULL,
   `tour_id` int(11) NOT NULL,
-  `point` int(11) NOT NULL
+  `point` int(11) NOT NULL,
+  `time` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Дамп данных таблицы `questions`
 --
 
-INSERT INTO `questions` (`id`, `question`, `ans1`, `ans2`, `ans3`, `ans4`, `write_ans`, `tour_id`, `point`) VALUES
-(1, '2 + 2 = ?', '2', '3', '4', '0', 3, 2, 10),
-(2, '3 + 3 = ?', '1', '2', '3', '6', 4, 2, 10),
-(3, '10 + 10 = ?', '20', '30', '40', '-50', 1, 2, 10),
-(4, '2 + 5', '1', '2', '3', '7', 4, 4, 10),
-(5, '3.docx', '--', '--', '--', '--', -1, 6, -1),
-(6, 'K + K', '2K', '1K', 'K', 'K', 1, 2, 1);
+INSERT INTO `questions` (`id`, `question`, `ans1`, `ans2`, `ans3`, `ans4`, `write_ans`, `tour_id`, `point`, `time`) VALUES
+(4, '3.txt', '--', '--', '--', '--', -1, 3, -1, 0),
+(2, '5+5', '10', '20', '30', '40', 1, 2, 10, 10000),
+(1, 'Сколько будет 2+2', '4', '5', '7', '9', 1, 2, 5, 10000),
+(5, 'Работает ли таймер?', 'Хз', 'Да', 'Надеюсь', 'Ес-но будет работать', 4, 2, 10, 60000);
 
 -- --------------------------------------------------------
 
@@ -172,8 +196,8 @@ CREATE TABLE IF NOT EXISTS `results` (
 --
 
 INSERT INTO `results` (`id`, `user_id`, `tour_id`, `points`, `state`, `adv`) VALUES
-(1, 1, 6, 5, 0, '1_6.txt'),
-(5, 1, 2, 10, 0, '--');
+(4, 1, 3, 99, 0, '1_1.txt'),
+(5, 1, 2, 15, 0, '--');
 
 -- --------------------------------------------------------
 
@@ -197,7 +221,7 @@ CREATE TABLE IF NOT EXISTS `settings` (
 --
 
 INSERT INTO `settings` (`id`, `site_name`, `site_describe`, `copyright`, `site_url`, `site_s`, `theme_name`, `language`) VALUES
-(0, 'Мудрёнок', 'Описание марафона', '© Тверская гимназия №10', 'http://localhost', '/home/site', 'classic2', 'ru');
+(0, 'Мудрёнок', 'Описание марафона', '© Тверская гимназия №10', 'http://localhost', '/home/site', 'mudrenok', 'ru');
 
 -- --------------------------------------------------------
 
@@ -252,4 +276,5 @@ INSERT INTO `users` (`id`, `login`, `pass`, `email`, `group`) VALUES
 (3, 'com2', 'c4ca4238a0b923820dcc509a6f75849b', 'com2@ya.ru', 0),
 (4, 'com3', 'c4ca4238a0b923820dcc509a6f75849b', 'com3@yandex.ruu', 0),
 (5, 'User', 'c4ca4238a0b923820dcc509a6f75849b', 'ys@yandex.ru', 0),
-(6, 'moder', '9ab97e0958c6c98c44319b8d06b29c94', 'moder@yandex.ru', 2);
+(6, 'moder', '9ab97e0958c6c98c44319b8d06b29c94', 'moder@yandex.ru', 2),
+(7, 'LosYear', 'f41f619a4529c74e406c55ac4d7870a3', 'losevyaroslav@gmail.com', 0);

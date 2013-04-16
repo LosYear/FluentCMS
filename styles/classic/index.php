5e037bd792d9f4ed3e27a185c72d66bf7464529f
Losev Yaroslav <losevyaroslav@gmail.com>
2013-04-16 19:54:08 +0400

    Deleting version 0.2 before merging with 0.3

--- styles/classic/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("./includes/functions.php");
-    require_once 'header.php';
-    require_once 'sidebar.php';
-    require_once 'page.php';
-    require_once 'footer.php';
-?>

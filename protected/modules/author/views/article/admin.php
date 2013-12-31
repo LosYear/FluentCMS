c235e0e6c4b9f0cd0c7b4a8c5c260fc4d574b6d2
Losev Yaroslav <losevyaroslav@gmail.com>
2014-01-01 00:36:41 +0400

    Small bugfixes. Fixed #39

--- protected/modules/author/views/article/admin.php
+++ protected/modules/author/views/article/admin.php
@@ -2,51 +2,53 @@
 /* @var $this ArticleController */
 /* @var $model Article */
 
-$this->breadcrumbs=array(
-	Yii::t('AuthorModule.admin', 'Articles')=>array('admin'),
-	Yii::t('AuthorModule.admin', 'Manage'),
+$this->breadcrumbs = array(
+    Yii::t('AuthorModule.admin', 'Articles') => array('admin'),
+    Yii::t('AuthorModule.admin', 'Manage'),
 );
 
-$this->menu=array(
-	array('label'=>Yii::t('author', 'Create article'), 'url'=>array('create'), 'icon' => 'file black',),
+$this->menu = array(
+    array('label' => Yii::t('author', 'Create article'), 'url' => array('create'), 'icon' => 'file black',),
 );
 ?>
 
 <div class="page-header">
-  <h1><?php echo Yii::t('AuthorModule.admin', 'Articles') ?> <small><?php echo Yii::t('AuthorModule.admin', 'Manage') ?></small></h1>
+    <h1><?php echo Yii::t('AuthorModule.admin', 'Articles') ?>
+        <small><?php echo Yii::t('AuthorModule.admin', 'Manage') ?></small>
+    </h1>
 </div>
 
 <?php
-				$issues = array();
-				$issue_model = new Issue;
-				$criteria = new CDbCriteria();
-				$criteria->condition = '`isOpened` = 1';
-				$criteria->order = '`id` DESC';
-				$tmp = $issue_model->findAll($criteria);
-				foreach ($tmp as $item) {
-					$issues[$item->id] = $item->number . '/' . $item->year;
-				}
-				
-				?>
+$issues = array();
+$issue_model = new Issue;
+$criteria = new CDbCriteria();
+$criteria->condition = '`isOpened` = 1';
+$criteria->order = '`id` DESC';
+$tmp = $issue_model->findAll($criteria);
+foreach ($tmp as $item) {
+    $issues[$item->id] = $item->number . '/' . $item->year;
+}
+
+?>
 <div id="main">
-<?php $this->widget('bootstrap.widgets.TbGridView', array(
-	'id'=>'article-grid',
-        'type'=>'striped bordered condensed',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
+    <?php $this->widget('bootstrap.widgets.TbGridView', array(
+        'id' => 'article-grid',
+        'type' => 'striped bordered condensed',
+        'dataProvider' => $model->search(),
+        'filter' => $model,
         //'template'=>"{items}{pager}",
-	'columns'=>array(
-			/*array('header'=>Yii::t('author', 'Issue'), 'value'=>'Issue::model()->findByPk(ArticleAdv::model()->findByPk($data->id)->issue_id)->number',
-			'filter' => $issues),*/
-			array('name' => 'issue_id', 'value' => 'Issue::model()->findByPk($data->advanced->issue_id)->number', 'header'=>Yii::t('author', 'Issue'), 'filter' => $issues),
-            array('name'=>'title', 'header'=>Yii::t('admin', 'Title')),
-            array('name'=>'url', 'header'=>Yii::t('admin', 'Url')),
+        'columns' => array(
+            /*array('header'=>Yii::t('author', 'Issue'), 'value'=>'Issue::model()->findByPk(ArticleAdv::model()->findByPk($data->id)->issue_id)->number',
+            'filter' => $issues),*/
+            array('name' => 'issue_id', 'value' => 'Issue::model()->findByPk($data->advanced->issue_id)->number', 'header' => Yii::t('author', 'Issue'), 'filter' => $issues),
+            array('name' => 'title', 'header' => Yii::t('admin', 'Title')),
+            array('name' => 'url', 'header' => Yii::t('admin', 'Url')),
             array('name' => 'created', 'header' => Yii::t('admin', 'Created')),
             array(
-                'name'=>'status', 
-                'type'=>'html',
-                'header'=>Yii::t('admin', 'Status'), 
-                'value'=>'($data->status == 1) ?  "<span class=\"glyphicon glyphicon-eye-open\"/>" : "<span class=\"glyphicon glyphicon-eye-close\"/>"',
+                'name' => 'status',
+                'type' => 'html',
+                'header' => Yii::t('admin', 'Status'),
+                'value' => '($data->status == 1) ?  "<span class=\"glyphicon glyphicon-eye-open\"/>" : "<span class=\"glyphicon glyphicon-eye-close\"/>"',
                 'filter' => array(
                     '0' => Yii::t('author', 'Draft'),
                     '1' => Yii::t('author', 'Published'),
@@ -55,9 +57,10 @@ $this->menu=array(
 
             ),
             array(
-                    'class'=>'bootstrap.widgets.TbButtonColumn',
-                    'htmlOptions'=>array('style'=>'width: 40px'),
-                    'template'=>'{update}{delete}',
-		),
-	)
-)); ?></div>
+                'class' => 'bootstrap.widgets.TbButtonColumn',
+                'htmlOptions' => array('style' => 'width: 40px'),
+                'template' => '{update}{delete}',
+                'visible' => 'Yii::app()->user->isAdmin() || $data->status != 1',
+            ),
+        )
+    )); ?></div>
